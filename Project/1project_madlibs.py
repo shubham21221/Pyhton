ae7ca516dd67837324b4c2643f46f3789373a63c
shubham <shubhamganeshsonawane11@gmail.com>
2024-12-31 15:51:10 +0530

    Add Project

---
+++ Project/1project_madlibs.py
@@ -0,0 +1,18 @@
+# youtuber = "me"
+
+# print("subscribe to " + youtuber)
+# print("subscribe to {}".format(youtuber))
+# print(f"subscribe to {youtuber}")
+
+# madlib project sample
+
+adj = input("adjective: ")
+verb1 = input("Verb: ")
+verb2 = input("Verb: ")
+famous_person = input("Famous person: ")
+
+madlib = f"Computer programming is so{adj}! It makes me so excited all the time because \
+    I love to {verb1}.stay hydrated and {verb2} like you are {famous_person}!"
+    
+print(madlib)
+
