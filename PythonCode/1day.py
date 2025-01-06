894b5e68a50a686882767caeaa9b20bb1bc6d55b
shubham <shubhamganeshsonawane11@gmail.com>
2025-01-06 15:20:29 +0530

    add

---
+++ PythonCode/1day.py
@@ -0,0 +1,27 @@
+#First Pyhon program
+print("hello wordl")
+
+#Expression
+x = 5
+y = 2
+z = x + y
+print(x)
+
+#run the command for terminal
+# python file_name.py
+
+#number
+#letters  // #characters
+#String
+
+num = 3
+letter = 'G'
+string = "the is a string"
+#this is a multiline comment
+multline = """this is 
+multi-line comment"""
+
+print(num)
+print(letter)
+print(string)
+print(multline)
---
+++ PythonCode/2day.py
@@ -0,0 +1,39 @@
+# Reserved Words
+
+'''multuline comment
+and       del       from      not       while
+as        elif      global    or        with
+assert    else      if        pass      yield
+break     except    import    print
+class     exec      in        raise
+continue  finally   is        return
+def       for       lambda    try '''
+
+# Variables
+variablename = "hello there!"
+sum = 5 + 10
+print(variablename)
+print(sum)
+
+#Assignment Statements
+my_variable = 123
+x = (123 + 200) * 2
+print(my_variable)
+print(x)
+#Numeric experssion
+x = 1+((2*(3**4))%2)
+print(x)
+
+#Types of value
+print(1+2)
+# >> "hello" ,"world!"
+print("hello" ,"world!")
+
+#find the veriable type 
+print(type("hello, "))
+
+# type convert
+print(float(9))
+print(str(7))
+print('hello ' + str(2))
+
